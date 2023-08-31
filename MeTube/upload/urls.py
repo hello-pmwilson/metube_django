bbea25c3c1ade6e9cec5b64214dd7c6b6bed79b5
hello-pmwilson <hello.pmwilson@gmail.com>
2023-08-30 21:00:23 -0400

    fixed username information on vscode

--- MeTube/upload/urls.py
+++ MeTube/upload/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'upload'
 urlpatterns = [
-    path("", views.upload, name="upload")
+    path("", views.upload, name="upload"),
+    path('upload_video', views.upload_video, name="upload_video")
 ]
 
--- MeTube/upload/views.py
+++ MeTube/upload/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import VideoUploadForm
+
 
 # Create your views here.
 def upload(request):
     return render(request, "upload/upload.html")
+
+def upload_video(request):
+    return render(request, "upload/upload_video.html")
+
+    # if request.method == 'Post':
+    #     form = VideoUploadForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(reverse('home:home'))
+    
+    # else:
+    #     form = VideoUploadForm()
+    #     return render(request, 'upload_video.html', {'form' : form})
+
